8da35cc5f6f2a335c07172fe9635b39effd6ad5c
itfgc <itfgc@itcast.cn>
2022-07-29 22:07:00 +0800

    首页路由初始化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-    App
-    <p>{{ $store.state.username }}</p>
-  </div>
+  <router-view />
 </template>
 
 <script setup>
+import request from "@/utils/request";
+
+const fn = () => {
+  // 测试请求
+  request('/member/profile', 'get', {a: 10})
+}
 
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,16 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
 // 路由规则
-const routes = []
+const routes = [
+  // 一级路由布局容器
+  {
+    path: '/',
+    component: () => import('@/views/Layout.vue'),
+    children: [
+      {path: '/', component: () => import('@/views/home/index.vue')}
+    ]
+  }
+]
 
 // vue2.0 new VueRouter({}) 创建路由实例
 // vue3.0 createRouter({}) 创建路由实例
