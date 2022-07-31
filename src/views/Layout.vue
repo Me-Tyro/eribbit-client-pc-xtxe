8f3e0dfec987cd1be80bda43e20a032a68d91f4c
itfgc <itfgc@itcast.cn>
2022-07-31 16:48:22 +0800

    左侧分类-结构渲染

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -2,20 +2,23 @@
   <!--  顶部通栏-->
   <app-navbar/>
   <!--  头部组件-->
-  <app-header />
+  <app-header/>
+  <!--  吸顶头部组件-->
+  <app-header-sticky/>
   <!--  内容容器-->
   <div class="app-body">
     <!--二级路由-->
     <router-view/>
   </div>
   <!--  底部组件-->
-  <app-footer />
+  <app-footer/>
 </template>
 
 <script setup>
 import AppNavbar from "@/components/app-navbar";
 import AppHeader from "@/components/app-header";
 import AppFooter from "@/components/app-footer";
+import AppHeaderSticky from "@/components/app-header-sticky";
 
 </script>
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="container">
-    HomePage
+    <!--    首页入口-->
+    <div class="home-entry">
+      <!--    左侧分类-->
+      <home-category />
+      <!--    轮播图-->
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "HomePage"
-}
+<script setup>
+import HomeCategory from "@/views/home/components/home-category";
 </script>
 
 <style scoped lang="less">
-.container {
-  .hoverShadow();
-}
+
 </style>
